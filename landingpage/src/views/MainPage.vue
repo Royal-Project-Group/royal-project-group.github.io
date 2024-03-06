e91cedc3cd2a22aa9a53616a95aaf24f6677b393
Niklas Paul <nisipa@web.de>
2024-03-06 13:12:31 +0100

    Add impressum

--- landingpage/src/views/MainPage.vue
+++ landingpage/src/views/MainPage.vue
@@ -135,7 +135,7 @@ const redirectToEditor = () => {
       <h2>Become part of the community or help in improving the Royal Tracer.</h2>
       <div>
         <div class="discord">
-          <a href="https://discord.gg/ekZGvJGx">
+          <a href="https://discord.gg/wm89TeRhee">
             <img src="/src/assets/discord.svg" />
           </a>
         </div>
@@ -147,6 +147,33 @@ const redirectToEditor = () => {
       </div>
     </section>
   </div>
+  <div class="content">
+    <section class='impressum'>
+      <h2>Impressum</h2>
+      <p>Angaben gemäß § 5 TMG</p>
+      <p>Johannes Möst <br> Heidenheimer Str. 78<br> 89075 Ulm <br> </p>
+      <p> <strong>Vertreten durch: </strong><br>
+Johannes Möst<br>
+Niklas Paul<br>
+Florian Mayer<br>
+Malte Lanz<br>
+Daniel Stempfle<br>
+Timo Pfaff<br>
+Marcel Treptow<br>
+      </p>
+      <p><strong>Kontakt:</strong> <br>
+E-Mail: <a href='mailto:moesjo01@thu.de'>moesjo01@thu.de</a><br>
+Telefon: +49-731 96537-700<br> <!-- Telefon vom SSC -->
+      </p>
+      <p><strong>Haftungsausschluss: </strong><br><br><strong>Haftung für Inhalte</strong><br><br>
+Die Inhalte unserer Seiten wurden mit größter Sorgfalt erstellt. Für die Richtigkeit, Vollständigkeit und Aktualität der Inhalte können wir jedoch keine Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.<br><br><strong>Haftung für Links</strong><br><br>
+Unser Angebot enthält Links zu externen Webseiten Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar. Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.<br><br><strong>Urheberrecht</strong><br><br>
+Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet. Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
+      </p><br> 
+      <p>Website Impressum erstellt durch <a href="https://www.impressum-generator.de">impressum-generator.de</a> von der <a href="https://www.kanzlei-hasselbach.de/" rel="nofollow">Kanzlei Hasselbach</a></p>
+    </section>
+ 
+</div>
 
 </template>
 <style scoped>
@@ -228,6 +255,10 @@ section.community img {
 
 div.discord {
   height: 100%;
+}
+
+section.impressum p {
+  font-size: 12px;
 }
 
 h1 {
